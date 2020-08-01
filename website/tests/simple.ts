2e1bee4c83a8d167c61d8b1f437acefb41000ab2
remorses <beats.by.morse@gmail.com>
2020-08-01 15:08:11 +0200

    website/tests/simple: delete the branch after test

--- website/tests/simple.ts
+++ website/tests/simple.ts
@@ -1,4 +1,4 @@
-import { createForkAndBranch } from '../pages/api/submit'
+import { createForkAndBranch, parseGithubUrl } from '../pages/api/submit'
 import assert from 'assert'
 import { Octokit } from '@octokit/rest'
 import { GITHUB_TOKEN } from '../constants'
@@ -9,10 +9,15 @@ const githubUrl = `https://github.com/remorses/edit-this-page`
 describe('github', () => {
     const octokit = new Octokit({ auth: GITHUB_TOKEN })
     it('createForkAndBranch', async () => {
+        const newBranchName = uuid.v4()
         const forkRes = await createForkAndBranch(octokit, {
             githubUrl,
-            newBranchName: uuid.v4(),
+            newBranchName,
         })
         console.log(forkRes)
+        await octokit.git.deleteRef({
+            ...parseGithubUrl(githubUrl),
+            ref: `heads/${newBranchName}`,
+        })
     })
 })

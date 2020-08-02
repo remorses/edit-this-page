918b224a5be703459d000243887d337243b4d772
remorses <beats.by.morse@gmail.com>
2020-08-02 18:35:34 +0200

    edit-this-page/src/constants: fix

--- edit-this-page/src/constants.ts
+++ edit-this-page/src/constants.ts
@@ -2,12 +2,11 @@ export const API_URL = isTesting()
     ? `http://localhost:3000/api/submit`
     : 'https://edit-this-page.now.sh/api/submit'
 
-    
 export const HANDLE_EDITS_GUIDE_URL = `http://edit-this-page.now.sh/docs`
 export const GITHUB_REPO = `https://github.com/remorses/edit-this-page`
 
 function isTesting() {
-    if (typeof process !== 'undefined' && !process?.env?.TESTING) {
+    if (typeof process !== 'undefined' && process?.env?.TESTING) {
         return true
     }
     return false

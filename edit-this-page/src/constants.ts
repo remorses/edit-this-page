994f4fc9e7000f2c86ade3561c14ed9c86c66528
remorses <beats.by.morse@gmail.com>
2020-08-02 18:35:11 +0200

    edit-this-page/src/constants: api url changes based on testing

--- edit-this-page/src/constants.ts
+++ edit-this-page/src/constants.ts
@@ -1,3 +1,14 @@
-export const API_URL = `http://localhost:3000/api/submit`
+export const API_URL = isTesting()
+    ? `http://localhost:3000/api/submit`
+    : 'https://edit-this-page.now.sh/api/submit'
+
+    
 export const HANDLE_EDITS_GUIDE_URL = `http://edit-this-page.now.sh/docs`
 export const GITHUB_REPO = `https://github.com/remorses/edit-this-page`
+
+function isTesting() {
+    if (typeof process !== 'undefined' && !process?.env?.TESTING) {
+        return true
+    }
+    return false
+}
